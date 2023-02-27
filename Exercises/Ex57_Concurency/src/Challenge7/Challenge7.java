dcf12c3a2a82a6822bde88838bb6d8a9e0dd3046
Adelin Pachedzhiev <56289229+Adelin-Pachedzhiev@users.noreply.github.com>
2023-02-27 08:32:04 +0200

    adding Concurrency challenge classes

--- Exercises/Ex57_Concurency/src/Challenge7/Challenge7.java
+++ Exercises/Ex57_Concurency/src/Challenge7/Challenge7.java
@@ -27,11 +27,15 @@ class BankAccount {
     public boolean withdraw(double amount) {
         if (lock.tryLock()) {
             try {
-                // Simulate database access
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
+                try {
+                    // Simulate database access
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                }
+                balance -= amount;
+            }finally {
+                lock.unlock();
             }
-            balance -= amount;
             System.out.printf("%s: Withdrew %f\n", Thread.currentThread().getName(), amount);
             return true;
         }
@@ -41,11 +45,15 @@ class BankAccount {
     public boolean deposit(double amount) {
         if (lock.tryLock()) {
             try {
-                // Simulate database access
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
+                try {
+                    // Simulate database access
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                }
+                balance += amount;
+            }finally {
+                lock.unlock();
             }
-            balance += amount;
             System.out.printf("%s: Deposited %f\n", Thread.currentThread().getName(), amount);
             return true;
         }
